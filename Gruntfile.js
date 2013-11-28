a5797c3bbb23593ecec428f756974861a36f1936
sndrs <sndrs@me.com>
2013-11-28 20:45:56 +0000

    fix missing images in dist

--- Gruntfile.js
+++ Gruntfile.js
@@ -115,7 +115,7 @@ module.exports = function (grunt) {
                 javascriptsDir: '<%= yeoman.app %>/scripts',
                 fontsDir: '<%= yeoman.app %>/styles/fonts',
                 importPath: '<%= yeoman.app %>/bower_components',
-                httpImagesPath: '/images',
+                httpImagesPath: '../images',
                 httpGeneratedImagesPath: '/images/generated',
                 httpFontsPath: '/styles/fonts',
                 relativeAssets: false,
